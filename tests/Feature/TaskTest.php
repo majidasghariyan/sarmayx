97349d46715d3e8e469871f502084a6dee542ef2
majid <majid@tarahionline.com>
2022-12-26 11:58:20 +0330

    add ide helper

---
+++ tests/Feature/TaskTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\User;
+use App\Task;
+
+class TaskTest extends TestCase
+{
+     /** @test */
+     public function test_create_product()
+     {
+        //  $user = User::factory()->create();
+        //  $token = $user->createToken('myapptoken')->plainTextToken;
+ 
+        //  $product = Product::factory()->make();
+ 
+ 
+        //  $response = $this->withToken($token)->json('post',route('majid'),[
+        //      'name' => $product->name,
+        //      'slug' => $product->slug,
+        //      'description' => $product->description,
+        //      'price' => $product->price
+        //  ]);
+         
+        //  $response->assertStatus(201);
+ 
+     }
+}
